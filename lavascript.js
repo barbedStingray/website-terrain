4556c8c8579d10cc7eab9dd90711e3afc30f5d5e
Ben Rehmann <brehmann@me.com>
2023-08-25 12:29:08 -0500

    function start

--- lavascript.js
+++ lavascript.js
@@ -1,14 +1,31 @@
 console.log('if you build it, they will play');
 
+function actualMath( num0, num1 ){
+    console.log( 'in actualMath', num0, num1 );
+    let answer = num0 + num1;
+    return answer;
+}
 
-let bigCities = [ '      chicago  ', '  miami', 'seattle ', 'sanfrancisco       ', '  chicago', 'boston', 'boston   ', 'atlanta '];
+function logger() {
+    console.log( 'in logger' );
+}// end logger
 
+function mathExample(){
+    console.log( 'in mathExample' );
+    let answer = 3 * 9;
+    return answer;
+}
 
-console.log( chicago.trim() );
-console.log( chicago );
-console.log( bigCities );
+function returner(){
+    console.log( 'in returner' );
+    return true;
+}
 
-
-
-
-
+logger();
+returner();
+console.log( 'running returner:', returner());
+console.log( 'running mathExample:', mathExample());
+console.log( 'running actualMath 4, 2', actualMath(4, 2));
+console.log( 'running actualMath 234, 123', actualMath(234, 123));
+console.log( 'running actualMath 82, 37', actualMath(82, 37));
+console.log( 'running actualMath 239, 082', actualMath(239, 82));
