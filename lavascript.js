bdce1eb68fd3f5ecbafb81d6850bade0ebb1fb91
Ben Rehmann <brehmann@me.com>
2023-08-25 11:36:37 -0500

    adjust

--- lavascript.js
+++ lavascript.js
@@ -1,43 +1,13 @@
 console.log('if you build it, they will play');
 
 
-let scenicSquares = [ 'plains', 'forest', 'desert', 'swamp', 'mountain'];
-let modularTerrain = ['shrubs', 'walls', 'fences', 'ruins'];
-let materials = ['flock', 'foamular', 'eXps'];
-
-let products = ['scenicSquares', 'modularTerrain', 'materials'];
-console.log( products );
-console.log( scenicSquares );
-console.log( modularTerrain );
-console.log( materials );
+let bigCities = [ '      chicago  ', '  miami', 'seattle ', 'sanfrancisco       ', '  chicago', 'boston', 'boston   ', 'atlanta '];
 
 
+console.log( chicago.trim() );
+console.log( chicago );
+console.log( bigCities );
 
-
-
-
-
-
-//create your list in html
-let squareList = document.querySelector('#square-list');
-for( let item of scenicSquares ){
-    console.log ( 'Scenic Style:', item);
-    squareList.innerHTML += '<div>' + item + '</div>'
-} //end html writing
-
-//create your modularTerrain list in html
-
-let modularList = document.querySelector('#modular-list');
-for( let item of modularTerrain ){
-    console.log( 'Style:', item );
-    modularList.innerHTML += '<div>' + item + '</div>'
-}// end html
-
-let materialList = document.querySelector('#material-list');
-for( let item of materials ){
-    console.log( 'Type:', item );
-    materialList.innerHTML += '<div>' + item + '</div>'
-}// end html
 
 
 
