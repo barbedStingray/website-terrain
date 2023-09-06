6ee3453625cb0c5e1fcba00b89f4cf7dbd7640fb
Ben Rehmann <brehmann@me.com>
2023-09-06 09:07:36 -0500

    buttons

---
+++ lavascript.js
@@ -0,0 +1,42 @@
+let hedgeCollection = [
+    {
+        item: `Green Hedges`,
+        image: `Hedges/Battlefield1.jpeg`,
+        price: 30.00,
+        pieces: 8,
+        flock: [`spring`, `mossy`, `desert`],
+    },
+    {
+        item: `Dry Hedges`,
+        image: `Hedges/6"pass.jpeg`,
+        price: 30.00,
+        pieces: 8,
+        flock: [`spring`, `mossy`, `desert`],
+    },
+    {
+        item: `Dark Hedges`,
+        image: `Hedges/TopView.jpeg`,
+        price: 30.00,
+        pieces: 8,
+        flock: [`spring`, `mossy`, `desert`],
+    },
+    {
+        item: `Berry Bushes`,
+        image: `Hedges/Battlefield4.jpeg`,
+        price: 30.00,
+        pieces: 8,
+        flock: [`spring`, `mossy`, `desert`],
+    }
+]// end array
+
+const addHedge = document.querySelector(`#disp-product`);
+
+function addTerrainProduct(arr){
+    for(let i=0; i<arr.length; i++){
+        addHedge.innerHTML += `<img class="hedge" src="${arr[i].image}"/>`;
+        console.log(`${arr[i].item}`);
+    }
+}// end addHedgeProduct
+
+// addTerrainProduct(hedgeCollection);
+// addTerrainProduct(hedgeCollection);
